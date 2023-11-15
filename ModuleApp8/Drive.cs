a139d371add80a229cdf00c780fa69f611cf5843
Nikita Nikita <jassie.jassie@yandex.ru>
2023-11-15 14:14:05 +0400

    Добавьте файлы проекта.

---
+++ ModuleApp8/Drive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleApp8
+{
+    internal class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+    }
+}
