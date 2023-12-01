0996f64139059f6569f68427805338ac8c4be2fd
Nikita Nikita <jassie.jassie@yandex.ru>
2023-12-01 18:01:49 +0300

    Юнит 4

---
+++ ModuleApp8/BinaryExperiment.cs
@@ -0,0 +1,43 @@
+﻿namespace ModuleApp8
+{
+    internal class BinaryExperiment
+    {
+        const string SettingsFileName = "Settings.cfg";
+
+        public static void WriteValues()
+        {
+            using (BinaryWriter writer = new(File.Open(SettingsFileName, FileMode.Create)))
+            {
+                writer.Write(20.666F);
+                writer.Write(@"Тестовая строка");
+                writer.Write(55);
+                writer.Write(false);
+            }
+        }
+        public static void ReadValues()
+        {
+            float FloatValue;
+            string StringValue;
+            int IntValue;
+            bool BooleanValue;
+
+            if (File.Exists(SettingsFileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+                {
+                    FloatValue = reader.ReadSingle();
+                    StringValue = reader.ReadString();
+                    IntValue = reader.ReadInt32();
+                    BooleanValue = reader.ReadBoolean();
+                }
+                Console.WriteLine("Из файла считано:");
+
+                Console.WriteLine("Дробь: " + FloatValue);
+                Console.WriteLine("Строка: " + StringValue);
+                Console.WriteLine("Целое: " + IntValue);
+                Console.WriteLine("Булево значение " + BooleanValue);
+            }
+        }
+
+    }
+}
